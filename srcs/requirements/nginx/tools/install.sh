9df1793c2ce9b739e51dcc8ff2eab58c51427963
n0ring <root@namina-debian.namina.42.fr>
2022-08-23 11:15:12 -0400

    some ref. don't down conts

--- srcs/requirements/nginx/tools/install.sh
+++ srcs/requirements/nginx/tools/install.sh
@@ -3,12 +3,9 @@
 apt update
 apt install -y vim nginx ufw openssl
 openssl genrsa -out ca.key 2048
-openssl req -new -key ca.key -out ca.csr -subj "/C=RU/ST=Moscow/L=Moscow/O=School 21/OU=IT Department/CN=namina.com"
+openssl req -new -key ca.key -out ca.csr -subj "/C=RU/ST=Moscow/L=Moscow/O=School 21/OU=IT Department/CN=namina.42.fr"
 openssl x509 -req -days 365 -in ca.csr -signkey ca.key -out ca.crt
 cp ca.crt /etc/ssl/certs/ 
 cp ca.key /etc/ssl/private/
 cp ca.csr /etc/ssl/private/
 cat conf > /etc/nginx/sites-enabled/default
-
-# rm /Dockerfile /conf
-# rm -rf /tools
--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -4,7 +4,6 @@
 if [ ! -f /var/www/html/wordpress/wp-config.php ];
 then
 
-date > /home/namina/log
 chown -R www-data:www-data /var/www/html/wordpress
 cp /conf /var/www/html/wordpress/wp-config.php 
 cat /www.conf > /etc/php/7.3/fpm/pool.d/www.conf
@@ -13,9 +12,7 @@ wp core install --allow-root --url=namina.42.fr\
  --title=namina.42.fr --admin_user=supervisor\
  --admin_password=strongpassword --admin_email=info@example.com\
  --path=/var/www/html/wordpress > resOfWPcore
-WORDPRESS_SETTED=true
 wp theme install --allow-root inspiro --activate --path=/var/www/html/wordpress/
-echo "start.sh end work" > /log 
 fi
 
 
