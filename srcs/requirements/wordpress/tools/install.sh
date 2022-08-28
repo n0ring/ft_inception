b61987c64844c98e2aadf257ab6e21745c659baf
n0ring <root@namina-debian.namina.42.fr>
2022-08-28 11:44:59 -0400

    FIX redis cache plugin bug

--- srcs/requirements/wordpress/tools/install.sh
+++ srcs/requirements/wordpress/tools/install.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
+apt update && apt install -y php-fpm php7.3 php-mysqli wget vim sendmail
 
-mkdir -p /var/www/html/wordpress
-mkdir -p /run/php
-touch /run/php/php7.3-fpm.pid
-
-apt update
-apt install -y php-fpm php7.3 php-mysqli wget vim mariadb-client sendmail
+cp www.conf /etc/php/7.3/fpm/pool.d/
 
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-cd /var/www/html/wordpress
-wp core download --allow-root
+
+
+
+
+# cd /var/www/html/wordpress
--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -2,26 +2,30 @@
 
 cd /var/www/html/wordpress
 
+chmod 755 -R /var/www/html/wordpress
+chown -R www-data:www-data /var/www/html/wordpress
+mkdir -p /run/php
+touch /run/php/php7.3-fpm.pid
+
+
 if [ ! -f /var/www/html/wordpress/wp-config.php ];
 then
-
-cp /conf /var/www/html/wordpress/wp-config.php 
+wp core download --allow-root
+mv /conf /var/www/html/wordpress/wp-config.php 
 chmod 777 /var/www/html/wordpress/wp-config.php
-cat /www.conf > /etc/php/7.3/fpm/pool.d/www.conf
+
 wp core install --allow-root --url=namina.42.fr\
  --title=namina.42.fr --admin_user=namina\
  --admin_password=password --admin_email=namina@42.fr
 
 wp user create --allow-root namina42   namina42@42.fr --role=author --user_pass=password
-# wp theme install --allow-root inspiro --activate --path=/var/www/html/wordpress/
-chown -R www-data:www-data /var/www/html/wordpress
+# # wp theme install --allow-root inspiro --activate --path=/var/www/html/wordpress/
 
-chmod 777 -R /var/www/html/wordpress
 
 wp plugin install redis-cache --activate --allow-root
 wp plugin update --all --allow-root
 # wp plugin activate redis-cache --allow-root
 fi
 
-wp redis enable --allow-root --force 
+wp redis enable --allow-root
 /usr/sbin/php-fpm7.3 --nodaemonize
